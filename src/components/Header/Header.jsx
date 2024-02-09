d3b9dad5672c59c1f4db476ea9a759418f716a60
AAlliissa <aliska05122002@yandex.ru>
2024-02-09 18:25:38 +0300

    eleven commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useTelegram} from './components/hooks/useTelegram';
+import {useTelegram} from '../hooks/useTelegram';
 import './Header.css';
 
 const Header = () => {
